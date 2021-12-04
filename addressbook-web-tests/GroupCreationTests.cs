8460efae664c4e0cdd75585ab41b1da3f5c29bde
Tatyana Bogatyreva <Tatyana.Bogatyreva@FLEXDATABASES.COM>
2021-12-04 19:35:33 +0300

    #Задание №3: Создать вспомогательные методы и классы

--- addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/GroupCreationTests.cs
@@ -30,7 +30,12 @@ namespace WebAddressbookTests
         {
             try
             {
+<<<<<<< HEAD
+                
+               driver.Quit();
+=======
                 driver.Quit();
+>>>>>>> aadfc800c5805b3b580f5ecf16efcf5b7906af34
             }
             catch (Exception)
             {
@@ -42,6 +47,71 @@ namespace WebAddressbookTests
         [Test]
         public void TheGroupCreationTests()
         {
+<<<<<<< HEAD
+            OpenHomePage();
+            Login(new AccountData ("admin","secret"));
+            GotoGroupsPage();
+            InitNewGroupCreation();
+            FillGroupForm(new GroupDate ("Test","1","2"));
+            SubmitGroupCreation();
+            ReturnToGroupPage();
+            Logout();
+        }
+
+        private void Logout()
+        {
+          driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void ReturnToGroupPage()
+        {
+          driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+           driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(GroupDate group)
+        {
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        private void InitNewGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GotoGroupsPage()
+        {
+           driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            driver.FindElement(By.Id("content")).Click();
+        }
+        
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+=======
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Clear();
             driver.FindElement(By.Name("user")).SendKeys("admin");
@@ -64,6 +134,7 @@ namespace WebAddressbookTests
             driver.FindElement(By.LinkText("group page")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
         }
+>>>>>>> aadfc800c5805b3b580f5ecf16efcf5b7906af34
         private bool IsElementPresent(By by)
         {
             try
