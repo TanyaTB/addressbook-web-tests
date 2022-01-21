512e207b80328da633538a17a4e7dbdb96dbb594
Tatyana Bogatyreva <Tatyana.Bogatyreva@FLEXDATABASES.COM>
2022-01-21 20:08:43 +0300

    Задание №12: реализовать тесты для проверки детальной информации о контакте

--- addressbook-web-tests/model/ContactsData.cs
+++ addressbook-web-tests/model/ContactsData.cs
@@ -9,24 +9,44 @@ namespace WebAddressbookTests
 {
     public class ContactsData : IEquatable<ContactsData>, IComparable<ContactsData>
     {
+       
+        public string firstname;
+        public string middlename = "";
+        public string lastname = "";
+        public string nickname = "";
+        public string title = "";
+        public string company = "";
+        public string address = "";
+        public string homephone = "";
+        public string mobilephone = "";
+        public string workphone = "";
+        public string fax = "";
+        public string mail1 = "";
+        public string mail2 = "";
+        public string mail3 = "";
+        public string homepage = "";
+        public string address2 = "";
+        public string phone2 = "";
+        public string notes = "";
+        public string bday = "";
+        public string bmonth = "";
+        public string byear = "";
+        public string aday = "";
+        public string amonth = "";
+        public string ayear = "";
+        public string group = "";
+        public string photo = "";
         private string allPhones;
-        private string allEmail;
         private string allDetails;
+        private string allEmail;
 
-        public ContactsData (string firstname,string middlename, string lastname, string address, string mobile, string email, string work, string home,string title)
+        public ContactsData(string firstname, string lastName)
         {
             FirstName = firstname;
-            LastName = lastname;
-            Address = address;
-            HomePhone = home;
-            MobilePhone = mobile;
-            WorkPhone = work;
-            Email = email;
-            Middlename = middlename;
-            Title = title;
-
-
+            LastName = lastName;
         }
+
+
         public bool Equals(ContactsData other)
         {
             if (Object.ReferenceEquals(other, null))
@@ -39,10 +59,12 @@ namespace WebAddressbookTests
             }
             return FirstName == other.FirstName && LastName == other.LastName;
         }
+
         public override int GetHashCode()
         {
             return LastName.GetHashCode() & FirstName.GetHashCode();
         }
+
         public int CompareTo(ContactsData other)
         {
             if (Object.ReferenceEquals(other, null))
@@ -60,24 +82,12 @@ namespace WebAddressbookTests
             return 0;
                 //LastName.CompareTo(other.LastName) & FirstName.CompareTo(other.FirstName);
         }
+
         public override string ToString()
         {
             return $"contact = {LastName} {FirstName}";
-        }
-
-        public ContactsData(string name)
-        {
-            FirstName = name;
-        }
-
-        public ContactsData(string firstname, string lastName )
-        {
-            FirstName = firstname;
-            LastName = lastName;
-
-
-
-        }
+        }  
+        
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -88,157 +98,43 @@ namespace WebAddressbookTests
         public string WorkPhone { get; set; }
         public string Email { get; set; }
         public string Id { get; set; }
+        public string Nickname { get; set; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+        public string Fax { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+        public string Homepage { get; set; }
+        public string BDay { get; set; }
+        public string BMonth { get; set; }
+        public string ADay { get; set; }
+        public string BYear { get; set; }
+        public string AMonth { get; set; }
+        public string AYear { get; set; }
+        public string Address2 { get; set; }
+        public string Phone2 { get; set; }
+        public string Notes { get; set; }
 
-
-        public string GetAge(string day, string month, string year, string fieldName)
+        public string AllPhones
         {
-            int monthNumber = 0;
-            int Age;
-            switch (month)
+            get
             {
-                case "January":
-                    monthNumber = 1;
-                    break;
-                case "February":
-                    monthNumber = 2;
-
-                    break;
-                case "March":
-                    monthNumber = 3;
-
-                    break;
-                case "April":
-                    monthNumber = 4;
-
-                    break;
-                case "May":
-                    monthNumber = 5;
-
-                    break;
-                case "June":
-                    monthNumber = 6;
-
-                    break;
-                case "July":
-                    monthNumber = 7;
-
-                    break;
-                case "August":
-                    monthNumber = 8;
-
-                    break;
-                case "September":
-                    monthNumber = 9;
-
-                    break;
-                case "October":
-                    monthNumber = 10;
-
-                    break;
-                case "November":
-                    monthNumber = 11;
-
-                    break;
-                case "December":
-                    monthNumber = 12;
-
-                    break;
-            }
-            if (year != "")
-            {
-                if ((DateTime.Now.Month >= monthNumber) && (DateTime.Now.Day >= Int32.Parse(day)))
-                    Age = DateTime.Now.Year - Int32.Parse(year);
-                else
-                    Age = DateTime.Now.Year - Int32.Parse(year) - 1;
-            }
-            else Age = 0;
-            string FullDate = "";
-            if (day != null && day != "-" && day != "0")
-            {
-                FullDate = day + ".";
-            }
-            if (month != null && month != "-")
-            {
-                if (FullDate != "")
+                if (allPhones != null)
                 {
-                    FullDate += " " + month;
+                    return allPhones;
                 }
                 else
                 {
-                    FullDate = month;
+                    return (CleanUp(HomePhone) + CleanUp(MobilePhone) + CleanUp(WorkPhone)).Trim();
                 }
+
             }
-            if (year != null && year != "")
+            set
             {
-                if (FullDate != "")
-                {
-                    FullDate += " " + year;
-                }
-                else
-                {
-                    FullDate = year;
-                }
+                allPhones = value;
             }
-            if (FullDate != "")
-            {
-                if (year != "")
-                {
-                    return fieldName + FullDate + " (" + Age + ")";
-                }
-                else
-                {
-                    return fieldName + FullDate;
-                }
-            }
-            else return "";
         }
-        public string GetAnniversary(string day, string month, string year, string fieldName)
-        {
-            int Anniversary;
-            if (year != "")
-                Anniversary = DateTime.Now.Year - Int32.Parse(year);
-            else
-                Anniversary = 0;
-            string FullDate = "";
-            if (day != null && day != "-" && day != "0")
-            {
-                FullDate = day + ".";
-            }
-            if (month != null && month != "-")
-            {
-                if (FullDate != "")
-                {
-                    FullDate += " " + month;
-                }
-                else
-                {
-                    FullDate = month;
-                }
-            }
-            if (year != null && year != "")
-            {
-                if (FullDate != "")
-                {
-                    FullDate += " " + year;
-                }
-                else
-                {
-                    FullDate = year;
-                }
-            }
-            if (FullDate != "")
-            {
-                if (year != null && year != "")
-                {
-                    return fieldName + FullDate + " (" + Anniversary + ")";
-                }
-                else
-                {
-                    return fieldName + FullDate;
-                }
-            }
-            else return "";
-        }
+       
         public string AllEmail
         {
             get
@@ -249,7 +145,7 @@ namespace WebAddressbookTests
                 }
                 else
                 {
-                    return (ReturnDetailwithBr(Email)  + ReturnDetailwithBr(Email2)  + ReturnDetailwithBr(Email3)).Trim();
+                    return (ReturnDetailwithRN(Email)  + ReturnDetailwithRN(Email2)  + ReturnDetailwithRN(Email3)).Trim();
                 }
             }
             set
@@ -258,25 +154,7 @@ namespace WebAddressbookTests
             }
         }
 
-        public string AllPhones
-        {
-            get
-            {
-                if (allPhones != null) 
-                {
-                    return allPhones;
-                }
-                else
-                {
-                    return (ReturnDetailwithRN(HomePhone) + ReturnDetailwithRN(MobilePhone) + ReturnDetailwithRN(WorkPhone)).Trim();
-                }
-                       
-            }
-            set
-            {
-                allPhones = value;
-            }
-        }
+       
 
         private string CleanUp(string phone)
         {
@@ -286,8 +164,6 @@ namespace WebAddressbookTests
             }
             return Regex.Replace(phone, "[ -()]", "")+ "\r\n"; 
 
-            //  (" ", "").Replace("-", "").Replace("(", "").Replace(")", "")+"\r\n";
-
         }
               
        
@@ -295,57 +171,33 @@ namespace WebAddressbookTests
         {
             get
             {
-                if (allDetails != null)
-                {
-                    return allDetails.Replace("\r", "").Replace("\n", "");
-                }
-                else
-                {
-                    return (ReturnFullName(FirstName, Middlename, LastName)
-                        + ReturnDetailwithBr(Title) 
-                        + ReturnDetailwithRN(Address) 
-                        + ReturnDetailwithBr(HomePhone, "H: ") 
-                        + ReturnDetailwithBr(MobilePhone, "M: ") 
-                        + ReturnDetailwithBr(WorkPhone, "W: ") 
-                        + ReturnDetailwithBr(Email) 
-                        + ReturnDetailwithBr(Email2) 
-                        + ReturnDetailwithBr(Email3) 
-                        + ReturnDetailwithoutBr(Notes)).Replace("\r","").Replace("\n", "");
+                return ((!string.IsNullOrEmpty(FirstName) ? $"{FirstName} " : string.Empty)
+                         + (!string.IsNullOrEmpty(Middlename) ? $"{Middlename} " : string.Empty)
+                         + (!string.IsNullOrEmpty(LastName) ? $"{LastName}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Nickname) ? $"{Nickname}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Title) ? $"{Title}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Company) ? $"{Company}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Address) ? $"{Address}\r\n\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(HomePhone) ? $"H: {HomePhone}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(MobilePhone) ? $"M: {MobilePhone}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(WorkPhone) ? $"W: {WorkPhone}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Fax) ? $"F: {Fax}\r\n\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Email) ? $"{Email}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Email2) ? $"{Email2}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Email3) ? $"{Email3}\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Homepage) ? $"Homepage:\r\n{Homepage}\r\n\r\n\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Address2) ? $"{Address2}\r\n\r\n" : string.Empty)
+                         + (!string.IsNullOrEmpty(Phone2) ? $"P: {Phone2}\r\n\r\n" : string.Empty)
+                         + Notes).Trim();
 
-                }
             }
+            
             set
             {
                 allDetails = value;
             }
         }
-
-        public string Nickname { get;set; }
-        public string Company { get;set; }
-        public string Title { get;set; }
-        public string Fax { get; set; }
-        public string Email2 { get; set; }
-        public string Email3 { get;  set; }
-        public string Homepage { get; set; }
-        public string BDay { get; set; }
-        public string BMonth { get; set; }
-        public string ADay { get; set; }
-        public string BYear { get; set; }
-        public string AMonth { get; set; }
-        public string AYear { get;  set; }
-        public string Address2 { get; set; }
-        public string Phone2 { get; set; }
-        public string Notes { get;  set; }
-
-        public string ReturnDetailwithBr(string text, string prefix = "")
-        {
-            if (text == null || text == "")
-            {
-                return "";
-            }
-            return prefix + text;
-
-        }
+            
         public string ReturnDetailwithRN(string text)
         {
             if (text == null || text == "")
@@ -355,58 +207,9 @@ namespace WebAddressbookTests
             return text + "\r\n";
 
         }
+  
 
-
-       // public string ReturnDetailwithBr(string text, string fieldName)
-       // {
-       //     if (text == null || text == "")
-        //    {
-        //        return "";
-        //    }
-        //    return text + "\r\n";
-      //  }
-
-        public string ReturnDetailwithoutBr(string text)
-        {
-            if (text == null || text == "")
-            {
-                return "";
-            }
-            return "\r\n\r\n" + text;
-        }
-
-
-        public string ReturnFullName(string name, string middlename, string lastname)
-        {
-            string FullName = "";
-            if (name != null && name != "")
-            {
-                FullName = name;
-            }
-            if (middlename != null && middlename != "")
-            {
-                if (FullName != "")
-                {
-                    FullName += " " + middlename;
-                }
-                else
-                {
-                    FullName = middlename;
-                }
-            }
-            if (lastname != null && lastname != "")
-            {
-                if (FullName != "")
-                {
-                    FullName += " " + lastname;
-                }
-                else
-                {
-                    FullName = lastname;
-                }
-            }
-
-            return FullName; 
+     
         } 
     }
   }
