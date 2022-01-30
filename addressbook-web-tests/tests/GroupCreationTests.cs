83ae8476e06692a866ad1521bc0beda07f7e002f
Tatyana Bogatyreva <Tatyana.Bogatyreva@FLEXDATABASES.COM>
2022-01-30 18:23:40 +0300

    Занятие 6. Работа с файлами Groups

--- addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/tests/GroupCreationTests.cs
@@ -4,7 +4,11 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WebAddressbookTests
 {
@@ -12,23 +16,75 @@ namespace WebAddressbookTests
     public class GroupCreationTests : AuthTestBase
     {
         public static IEnumerable<GroupData> RandomGroupDataProvider()
-        {
-            List<GroupData> groups = new List<GroupData>();
-            for (int i = 0; i < 5; i++)
+      {
+           List<GroupData> groups = new List<GroupData>();
+           for (int i = 0; i < 5; i++)
             {
                 groups.Add(new GroupData(GenerateRandomString(30))
-                {
-                    Header = (GenerateRandomString(100)),
-                    Footer = (GenerateRandomString(100))
+               {
+                   Header = (GenerateRandomString(100)),
+                   Footer = (GenerateRandomString(100))
+               });
+            }
+            return groups;
+       }
 
+        public static IEnumerable<GroupData> GroupDataFromCsvFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+           string[] lines= File.ReadAllLines(@"group.csv");
+            foreach(string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
                 });
             }
             return groups;
         }
+        public static IEnumerable<GroupData> GroupDataFromXmlFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            return (List<GroupData>) new XmlSerializer(typeof(List<GroupData>)).Deserialize(new StreamReader(@"groups.xml"));
+            
+        }
+        public static IEnumerable<GroupData> GroupDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<GroupData>>(File.ReadAllText(@"groups.json"));
+
+        }
+
+        public static IEnumerable<GroupData> GroupDataFromExcelFile()
+        {
+
+            List<GroupData> groups = new List<GroupData>();
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb =  app.Workbooks.Open(Path.Combine(Directory.GetCurrentDirectory(), @"groups.xlsx"));
+            Excel.Worksheet sheet = wb.ActiveSheet;
+            Excel.Range range = sheet.UsedRange;
+            for( int i= 1; i <= range.Rows.Count; i++)
+            {
+                groups.Add(new GroupData()
+                {
+                    Name =range.Cells[i, 1].Value,
+                    Header = range.Cells[i, 2].Value,
+                    Footer = range.Cells[i, 3].Value
+
+                } );
+            }
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
+            return groups;
+
+
+        }
 
 
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        [Test, TestCaseSource("GroupDataFromExcelFile")]
         public void TheGroupCreationTests(GroupData groupData)
         {
             //GroupData groupData = new GroupData("Test");
